3f35736fd1c172682d39874f18d39ba1d9f636ed
Sabik52 <rahman52neajur@gmail.com>
2022-04-23 22:38:36 +0600

    sign in out added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useFirebase from '../../hooks/useFirebase';
 import './Header.css'
 
 const Header = () => {
+    const {user, handleSingOUt} = useFirebase();
     return (
         <div className='header'>
            <nav>
            <Link to='/'>Home</Link>
-            <Link to='/login'>Login</Link>
+           <Link to='/products'>Products</Link>
+           <Link to='/oreders'>Orders</Link>
+           <Link to='/register'>Register</Link>
+           { 
+           user ?.uid
+            ?
+           <button onClick={handleSingOUt}>signout</button>
+           :
+           <Link to='/login'>Login</Link>
+           }
+           <span>{user?. displayName && user.displayName}</span>
            </nav>
         </div>
     );
